803887489a8ebb12a315479ac31df7a1bc709e1f
MrNazeer <nazeerahamed1724@gmail.com>
2022-11-27 16:54:19 +0530

    2 commit

--- src/components/TodoInput/Todoinput.js
+++ src/components/TodoInput/Todoinput.js
@@ -1,41 +1,54 @@
-import React, { useState } from 'react'
-import { TodoList } from '../TodoList/Todolist';
-import "./Style.scss"
+import React, { useState } from "react";
+import { TodoList } from "../TodoList/Todolist";
+import "./Style.scss";
 
 function Form() {
   function handleSubmit(e) {
     e.preventDefault();
     console.log("Clicked submit");
   }
-
 }
 
-
-export  function Todoinput() {
-  const [value, setValue] = useState()
-  const [items, setItems] = useState([])
+export function Todoinput() {
+  const [value, setValue] = useState("");
+  const [items, setItems] = useState([]);
 
   const submit = (e) => {
-    e.preventDefault()
-    setItems((pre) => [...pre, value])
+    e.preventDefault();
+    setItems((pre) => [...pre, value]);
     // items.push(value)
-    console.log(items, "items-----");
-  }
+    // console.log(items, "items-----");
+    // clear
+    setValue("");
+  };
 
-  const onChange =  (e) => {
-    setValue(e.target.value) 
-  }
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+
 
   return (
-    <div className='input-container'>
+    <div className="input-container">
       {/* {items} */}
       <form onSubmit={Form} className="form">
-        <input type="text" className='todo-input' placeholder='Enter a Todo..' onChange={onChange} />
-        <input type="submit" value="Add" className='todo-btn' onClick={submit} />
+        <input
+          type="text"
+          className="todo-input"
+          placeholder="Enter a Todo.."
+          value={value}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Add"
+          className="todo-btn"
+          onClick={submit}
+        />
       </form>
-    {items.map((data,index) => (
-      <TodoList key={index.toString()}  value = {data} />
-    ))}
+      {items.map((data, index) => (
+        <TodoList key={index.toString()} value={data}  items = {items} setItems = {setItems} />
+      ))}
     </div>
-  )
+  );
 }
--- src/components/TodoList/Todolist.js
+++ src/components/TodoList/Todolist.js
@@ -1,18 +1,31 @@
-import React from 'react'
-import "./Style.scss"
+import React from "react";
+import "./Style.scss";
 import { AiFillDelete } from "react-icons/ai";
 import { AiFillEdit } from "react-icons/ai";
-export  function TodoList(data) {
-  const {value} = data
+export function TodoList({ value, items, setItems }) {
+
+console.log(items);
+
+const del = () => {
+  const list = items.filter((elements) => {
+    return elements !== value
+  })
+  setItems(list)
+};
+
   return (
-  <>
-  <li className='list'>{value}
-  <div className='wraper'>
-  <span><AiFillDelete /></span>
-  <span><AiFillEdit /></span>
-  </div>
-  </li>
-  
-  </>
-  )
+    <>
+      <li className="list">
+        {value}
+        <div className="wraper">
+          <span>
+            <AiFillDelete onClick={del} />
+          </span>
+          <span>
+            <AiFillEdit />
+          </span>
+        </div>
+      </li>
+    </>
+  );
 }
